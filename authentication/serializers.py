ad9d623539e91aa29937a280adbf3e535c099a04
Tharindu209 <tharindudamruwan23@gmail.com>
2024-03-02 14:01:38 +0530

    auth

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -3,20 +3,20 @@ from django.contrib.auth.models import User
 
 
 class Registration_serializers(serializers.ModelSerializer):
-    password2 = serializers.CharField(style={'input_type':'password'},write_only=True)
+    password2 = serializers.CharField(style={"input_type": "password"}, write_only=True)
+
     class Meta:
         model = User
-        fields = ['username','email','password','password2']
-        extra_kwargs = {
-            'password':{'write_only':True}
-        }
-
+        fields = ["username", "email", "password", "password2"]
+        extra_kwargs = {"password": {"write_only": True}}
 
     def validate(self, attrs):
-        password=attrs.get('password')
-        password2=attrs.pop('password2')
+        password = attrs.get("password")
+        password2 = attrs.pop("password2")
         if password != password2:
-            raise serializers.ValidationError("Password and Confirm Password Does not match")
+            raise serializers.ValidationError(
+                "Password and Confirm Password Does not match"
+            )
         return attrs
 
     def create(self, validate_data):
